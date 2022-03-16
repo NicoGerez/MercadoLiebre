648ea5aa79becc7c424e6c7f59f24cd10aca3404
NicoGerez <gerez.nicolas999@gmail.com>
2022-03-15 22:28:17 -0300

    donde esta mi estilo

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath) );
 
+
 app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto 3030");
 });
