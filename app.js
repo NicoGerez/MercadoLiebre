1afbe2862477e1fdc7db2552c95d2e8fc48df856
NicoGerez <gerez.nicolas999@gmail.com>
2022-03-15 22:45:02 -0300

    intento 4

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath) );
-
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto 3030");
